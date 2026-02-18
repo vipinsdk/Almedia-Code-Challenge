10baadc804a774ac3f09f9777bf55899093bb8fd
Vippin Kumar Jeetmal <vije01@dfki.de>
2026-02-18 05:02:13 +0100

    Fixed the PORT env name (DB_PORT)

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,4 +1,4 @@
-import * as dotenv from 'dotenv';
+import dotenv from 'dotenv';
 dotenv.config();
 
 export const Config = {
@@ -7,7 +7,7 @@ export const Config = {
     user: process.env.DB_USER || 'postgres',
     password: process.env.DB_PASS || 'password',
     name: process.env.DB_NAME || 'offers_db', 
-    port: parseInt(process.env.PORT || '3000', 10),
+    port: parseInt(process.env.DB_PORT || '5432', 10),
   },
   providers: {
     offer1: process.env.OFFER1_URL || 'http://localhost:3000/offers1',
